1f9f17c0371d78c4f50882f177e3322c524ea4e9
Charles NDAYISABA <nccharles1@gmail.com>
2021-10-12 16:32:12 +0200

    initial

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useState } from "react";
 
-function App() {
+const App = () => {
+  const [program, setProgram] = useState([]);
+  const [name, setName] = useState("");
+  const onSubmit = (e) => {
+    e.preventDefault();
+    if(name===""){
+    return  alert("Please Enter the Program")
+    }
+    e.target.reset();
+    const newArr = [name];
+    setName("");
+    setProgram((p) => [...p, newArr]);
+  };
+  const onChange = (e) => {
+    setName(e.target.value);
+  };
+  const Array = program.map((i) => <li>{i}</li>);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={onSubmit}>
+        <label>Programme: </label>
+        <input type="text" name="name" autoFocus={true} onChange={onChange} />
+        <button>Ajouter</button>
+      </form>
+      <h1>Liste des programmes: </h1>
+      <ul>{Array}</ul>
     </div>
   );
-}
+};
 
 export default App;
